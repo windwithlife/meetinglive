c2be575e6cefd7d579c084dcea8089a9351be803
fzhange <2861669535@qq.com>
2020-07-22 16:12:52 +0800

    发布配置文件修改

--- web/operation_platform_web/pm2.config.js
+++ web/operation_platform_web/pm2.config.js
@@ -7,13 +7,24 @@ module.exports = {
         ignore_watch:['node_modules','logs','public'],
         node_args: '--harmony', // node的启动模式
         env_production: {
-            NODE_ENV: 'production',
-            PORT: 8080
+            NODE_ENV: 'production'
         },
         out_file: path.join(__dirname,'./logs/out.log'), // 普通日志路径
         error_file: path.join(__dirname,'./logs/error.log'), // 错误日志路径
         merge_logs: true,
-        log_date_format: 'YYYY-MM-DD HH:mm Z',
-        instances:"1" //负载均衡   根据机器CPU核数，开启对应数目的进程运行项目
+        log_date_format: 'YYYY-MM-DD HH:mm:ss',
+        instances:"1", //负载均衡   根据机器CPU核数，开启对应数目的进程运行项目
+        // exec_mode : "cluster"
     }]
 }
+
+/**
+ * pm2 start
+ * pm2 show (appname|id) 查看详细状态信息
+ * pm2 monit 监控每个 node 进程的 cpu 和内存使用情况
+ * pm2 logs
+ * pm2 logs APP-NAME       # Display APP-NAME logs 
+ * pm2 web 监控运行这些进程的机器的状态
+ * pm2 describe <id|app_name>
+ * pm2 reload all
+ */

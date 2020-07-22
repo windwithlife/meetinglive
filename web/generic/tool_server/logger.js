757e0bd9ffe46220ed2e1f6d0676494240caee13
fzhange <2861669535@qq.com>
2020-07-22 19:14:10 +0800

    删除generic

--- web/generic/tool_server/logger.js
+++
@@ -1,8 +0,0 @@
-const logger = require('log4js')
-function createLogger(filePath, level) {
-  const log = logger.getLogger(filePath)
-  log.level = level || 'debug'
-  return log
-}
-
-module.exports = createLogger

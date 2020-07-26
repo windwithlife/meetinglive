5e103c2ebf455971b4cc5adb4b95d305b92ff566
fzhange <2861669535@qq.com>
2020-07-26 13:08:11 +0800

    提交代码

--- web/operation_platform_web/common/index.js
+++ web/operation_platform_web/common/index.js
@@ -1,10 +1,25 @@
 import axios from "axios";
 const host_server_base_path = 'https://service.koudaibook.com/meeting-server'
 
+
 export async function invoke_post(url,params={}){
+    axios.defaults.withCredentials=true;
+    axios.defaults.crossDomain=true;
+const payload = {
+    platType:4,
+    category:1,
+    version:1,
+    platForm:"web",
+    token:"token",
+    data:params,
+}
+
+console.log('JSON.stringify(payload): ', JSON.stringify(payload));
     try{
         let result = await axios({
-            // console.log('result: ', result);
+            headers: {
+                'Content-Type': 'application/json'
+            },
             method: 'post',
             url:`${host_server_base_path}/${url}`,
             data: {
@@ -18,7 +33,7 @@ export async function invoke_post(url,params={}){
         })
         return result;
     }catch(error){
-        console.error('error: ', error);
+        console.error('---invoke_post_error---: ', error);
         return {}
     }
 }
